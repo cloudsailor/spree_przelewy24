720b6ccb27720cf51249679737db24fa801d2b55
Patryk Katek <pkatek@CLDSLRMBP231.local>
2023-11-16 21:47:15 +0100

    Update spree p24 to new front

--- lib/spree_przelewy24/engine.rb
+++ lib/spree_przelewy24/engine.rb
@@ -7,26 +7,36 @@ module SpreePrzelewy24
 
     config.autoload_paths += %W[#{config.root}/lib]
 
+    config.after_initialize do |app|
+      app.config.spree.payment_methods << Spree::Gateway::Przelewy24
+    end
+
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
+      if self.frontend_available?
+        Dir.glob(File.join(File.dirname(__FILE__), '../../lib/spree_frontend/controllers/spree/*_decorator*.rb')) do |c|
+          Rails.application.config.cache_classes ? require(c) : load(c)
+        end
       end
 
-      Rails.application.config.after_initialize do
-        Rails.application.config.spree.payment_methods << Spree::Gateway::Przelewy24
-      end
-      # puts Rails.application.config.spree.payment_methods.inspect
-      # ::Rails.application.config.spree.payment_methods << Spree::Gateway::Przelewy24
       Spree::PermittedAttributes.source_attributes << :payment_method_name
       Spree::PermittedAttributes.source_attributes << :issuer
-      # Spree::Api::ApiHelpers.payment_source_attributes << :payment_method_name
-      # Spree::Api::ApiHelpers.payment_source_attributes << :issuer
-      # Spree::Api::ApiHelpers.payment_source_attributes << :payment_url
 
       # Orders should be shippable whenever they're authorized
       Spree::Config[:auto_capture_on_dispatch] = true
     end
 
+    def self.frontend_available?
+      @@frontend_available ||= ::Rails::Engine.subclasses.map(&:instance).map{ |e| e.class.to_s }.include?('Spree::Frontend::Engine')
+    end
+
+    paths['app/controllers'] << 'lib/controllers'
+
+    if self.frontend_available?
+      paths["app/controllers"] << "lib/spree_frontend/controllers"
+      paths["app/views"] << "lib/views/frontend"
+    end
+
     config.to_prepare &method(:activate).to_proc
   end
+
 end
