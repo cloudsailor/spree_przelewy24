74c7714c980edfa522d5ee515a514279862cc19e
Patryk Katek <pkatek@CLDSLRMBP231.local>
2024-01-02 20:30:50 +0100

    Added p24 cancel

---
+++ app/models/spree/payment/processing_decorator.rb
@@ -0,0 +1,19 @@
+module Spree::Payment::ProcessingDecorator
+
+  def cancel!
+    if payment_method.is_a? Spree::Gateway::Przelewy24
+      cancel_with_p24
+    else
+      super
+    end
+  end
+
+  private
+
+  def cancel_with_p24
+    response = payment_method.cancel(payment_id)
+    handle_response(response, :void, :failure)
+  end
+end
+
+Spree::Payment.include(Spree::Payment::ProcessingDecorator)
